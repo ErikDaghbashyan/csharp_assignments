64aed2860293460ce5bb7e89c2b5c6b14b00e94c
erikd <139626332+ErikDaghbashyan@users.noreply.github.com>
2023-07-22 21:53:26 +0400

    Implemented Complex Number

---
+++ ComplexNumber/ComplexNumber/ComplexNumber.cs
@@ -0,0 +1,47 @@
+﻿namespace ComplexNumber
+{
+    internal class ComplexNumber
+    {
+        public double RealPart { get; set; }
+        public double ImaginaryPart { get; set; }
+        public ComplexNumber() :this(1,1){ }
+        public ComplexNumber(double realPart, double imaginaryPart)
+        {
+            RealPart = realPart;
+            ImaginaryPart = imaginaryPart;
+        }
+        public double AbsoluteValue()
+        {
+            return Math.Sqrt(RealPart* RealPart+ImaginaryPart*ImaginaryPart);
+        }
+        public static ComplexNumber operator+(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.RealPart+b.RealPart,a.ImaginaryPart+b.ImaginaryPart);
+        }
+        public static ComplexNumber operator -(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.RealPart - b.RealPart, a.ImaginaryPart - b.ImaginaryPart);
+        }
+        public static ComplexNumber operator *(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.RealPart * b.RealPart, a.ImaginaryPart * b.ImaginaryPart);
+        }
+        public static implicit operator ComplexNumber(double n)
+        {
+            return new ComplexNumber(n, 0);
+        }
+        public static explicit operator double(ComplexNumber cn)
+        {
+            return cn.RealPart;
+        }
+        public override string ToString()
+        {
+            //if (ImaginaryPart < 0)
+            //    if (ImaginaryPart == -1) return $"{RealPart}-i";
+            //    else  return $"{RealPart}{ImaginaryPart}i";
+            //else if (ImaginaryPart == 1)return $"{RealPart}+i";
+            //else  return $"{RealPart}+{ImaginaryPart}i";
+            return $"( {RealPart} , {ImaginaryPart} )";
+        }
+    }
+}
---
+++ ComplexNumber/ComplexNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ComplexNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNumber cn = new ComplexNumber(-8,12);
+            Console.WriteLine(cn.ToString());
+            Console.WriteLine($"Absolute Value: {cn.AbsoluteValue():F2}");
+            ComplexNumber sum = cn + cn;
+            Console.WriteLine(sum.ToString());
+            double number = (double)sum;
+            Console.WriteLine(number);
+            double num1 = 5.9;
+            ComplexNumber implicted = new ComplexNumber();
+            implicted = num1;
+            Console.WriteLine(implicted.ToString());
+        }
+    }
+}
